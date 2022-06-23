a6b0861ad42b8713ab0ba7dcf63a167b8da0fec6
Rene Ramirez <rdejramirez@gmail.com>
2022-06-23 05:40:39 -0500

    12 tests and 12 assertions passed

--- app/Number.php
+++ app/Number.php
@@ -5,19 +5,29 @@ namespace App;
 class Number
 {
 	protected $random_number;
+	protected static $calledNumbers = [];
 
 	public function __construct()
 	{
-		$this->random_number = rand(1, 75);
-	}
+		$actualNumber = rand(1, 75);
 
-	public function setRandomNumber()
-	{
-		$this->random_number = rand(1, 75);
+		foreach(Number::$calledNumbers as $calledNumber){
+			while($calledNumber === $actualNumber) {
+				$actualNumber = rand(1, 75);
+			}
+		}
+		array_push(Number::$calledNumbers, $actualNumber);
+		
+		$this->random_number = $actualNumber;
 	}
 
 	public function getRandomNumber()
 	{
 		return $this->random_number;
+	}
+
+	public function setRandomNumber($random_number)
+	{
+		$this->random_number = $random_number;
 	}
 }
